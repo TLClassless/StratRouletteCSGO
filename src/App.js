5bf05cf55141010f9e11a955912e8b0ce5158e35
Ben <classle55sc2@gmail.com>
2020-11-16 22:23:10 +0000

    git init

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/TLClassless/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Created with ❤️ by TLC*
         </a>
       </header>
     </div>
