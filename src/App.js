85ddcd76b13c707e59289a18711a08f5384142b5
Ben <classle55sc2@gmail.com>
2020-12-21 02:38:32 +0000

    added axios functionality to pull strats.json from remote API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
-  myfunction() {
-    console.log("CLICKED");
+  state = {
+    strats: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://tlclassless.github.io/stratRouletteAPI/Data/strats.json`)
+      .then((res) => {
+        const strat = res.data.strats;
+        const stratSelect = strat[Math.floor(Math.random() * strat.length)];
+        console.log(stratSelect);
+        this.setState({ stratSelect });
+      });
   }
 
   render() {
     return (
-      <div className="App">
+      <div className="App ct">
         <h1 className="title">Strat Name</h1>
-        <h3 className="description">
+        <h3 className="description cont-3rd-width">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </h3>
-        <input type="submit" value="New Strat" className="btn btn-new" />
-        <button>
-          <img
-            src="../../public/logo512.png"
-            alt="CT/T Switch"
-            onClick={this.myfunction}
-          />
-        </button>
+        <div>
+          <input type="submit" value="New Strat" className="btn btn-new" />
+          <button>
+            <img src="../../public/logo512.png" alt="CT/T Switch" />
+          </button>
+        </div>
       </div>
     );
   }
