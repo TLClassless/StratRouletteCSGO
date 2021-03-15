547aa59a2a5fd01378470947e6ebf8e3df538228
Ben <classle55sc2@gmail.com>
2021-03-15 15:00:23 +0000

    fixed styling

--- src/App.js
+++ src/App.js
@@ -23,22 +23,24 @@ class App extends Component {
     const isCT = this.state.isCT;
     console.log(isCT);
     return (
-      <div className="App">
-        <div className={isCT ? "ct" : "t"}>
-          <h1 className="title">{this.state.name}</h1>
-          <h3 className="description cont-3rd-width">{this.state.desc}</h3>
-          <div className="btn-div">
-            <input
-              type="submit"
-              value="New Strat"
-              className="btn strat-btn ct-btn"
+      <div className={isCT ? "App ct" : "App t"}>
+        <h1 className="title">{this.state.name}</h1>
+        <h3 className="description cont-3rd-width">{this.state.desc}</h3>
+        <div className="btn-div">
+          <input
+            type="submit"
+            value="New Strat"
+            className={isCT ? "ct-btn btn strat-btn" : "t-btn btn strat-btn"}
+          />
+          <div
+            className="switchSides"
+            onClick={() => this.setState({ isCT: !isCT })}
+          >
+            <img
+              src="./ctt.png"
+              alt="CT/T Switch"
+              className="btn switchSides"
             />
-            <div
-              className="switchSides"
-              onClick={() => this.setState({ isCT: !isCT })}
-            >
-              <img src="./ctt.png" alt="CT/T Switch" className="btn" />
-            </div>
           </div>
         </div>
       </div>
