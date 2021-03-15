c285bc0dbb7ff35fb0ed844d3257c74617025509
Ben <classle55sc2@gmail.com>
2021-03-15 14:39:42 +0000

    isCT state addded, background switches between CT/T color on click

--- src/App.js
+++ src/App.js
@@ -15,20 +15,31 @@ class App extends Component {
       });
   }
 
-  state = {};
+  state = {
+    isCT: true,
+  };
 
   render() {
+    const isCT = this.state.isCT;
+    console.log(isCT);
     return (
-      <div className="App ct">
-        <h1 className="title">{this.state.name}</h1>
-        <h3 className="description cont-3rd-width">{this.state.desc}</h3>
-        <div className="btn-div">
-          <input
-            type="submit"
-            value="New Strat"
-            className="btn strat-btn ct-btn"
-          />
-          <img src="./ctt.png" alt="CT/T Switch" className="btn" />
+      <div className="App">
+        <div className={isCT ? "ct" : "t"}>
+          <h1 className="title">{this.state.name}</h1>
+          <h3 className="description cont-3rd-width">{this.state.desc}</h3>
+          <div className="btn-div">
+            <input
+              type="submit"
+              value="New Strat"
+              className="btn strat-btn ct-btn"
+            />
+            <div
+              className="switchSides"
+              onClick={() => this.setState({ isCT: !isCT })}
+            >
+              <img src="./ctt.png" alt="CT/T Switch" className="btn" />
+            </div>
+          </div>
         </div>
       </div>
     );
